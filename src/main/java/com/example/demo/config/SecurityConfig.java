58b59ea74d1863b12c119c328ebdd7fd26e13705
ZengXinLei <46377981+ZengXinLei@users.noreply.github.com>
2021-07-02 05:09:23 +0800

    SecurityConfig设置

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -2,15 +2,20 @@ package com.example.demo.config;
 
 import com.example.demo.service.ZUserService;
 import com.example.demo.service.impl.UserDetailsServiceImpl;
+import com.example.demo.util.R;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.io.PrintWriter;
 
 /**
  * @className: com.example.demo.config.SecurityConfig
@@ -19,12 +24,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  * @create: 2021-07-02 0:47
  */
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserDetailsServiceImpl zUserService;
+
     @Bean
-    PasswordEncoder passwordEncoder(){
+    PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
@@ -41,10 +48,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/**").hasRole("USER")
-                .anyRequest().authenticated()
+//                .antMatchers("/**").hasRole("USER")
+
+                .anyRequest().permitAll()
                 .and()
-                .formLogin().usernameParameter("name")
-        .permitAll();
+                .formLogin()
+                .permitAll()
+                .successHandler(((httpServletRequest, httpServletResponse, authentication) -> {
+                    PrintWriter writer = httpServletResponse.getWriter();
+                    new ObjectMapper().writeValue(writer, R.ok("成功了"));
+                    writer.flush();
+                    writer.close();
+                }))
+                .failureHandler(((httpServletRequest, httpServletResponse, e) -> {
+                    PrintWriter writer = httpServletResponse.getWriter();
+                    new ObjectMapper().writeValue(writer, R.error());
+                    writer.flush();
+                    writer.close();
+                }))
+
+
+                .loginProcessingUrl("/login")
+                .usernameParameter("name")
+                .passwordParameter("password")
+                .and()
+                .csrf().disable().exceptionHandling()
+        ;
+//        .successHandler();
     }
 }
