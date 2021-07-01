eddfc0cfca229664fedd1a9ad65c44565eecff42
ZengXinLei <46377981+ZengXinLei@users.noreply.github.com>
2021-07-01 15:15:14 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,11 @@
 <template>
   <div id="app">
-
-
-    <Index></Index>
-
+    <router-view />
   </div>
 </template>
 
 <script>
-import Index from '@/views/index/Index'
-
 export default {
-  name: 'App',
-  components: {
-    Index
-  }
+  name: 'App'
 }
 </script>
-
-<style>
-#app {
-  /*font-family: Avenir, Helvetica, Arial, sans-serif;*/
-  /*-webkit-font-smoothing: antialiased;*/
-  /*-moz-osx-font-smoothing: grayscale;*/
-  /*text-align: center;*/
-  /*color: #2c3e50;*/
-  /*margin-top: 60px;*/
-}
-
-</style>
