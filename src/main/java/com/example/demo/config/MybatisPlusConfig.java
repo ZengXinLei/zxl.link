f2bc09c18abd5b276c20be6a52ec6c9a0519fab1
ZengXinLei <46377981+ZengXinLei@users.noreply.github.com>
2021-07-03 22:16:29 +0800

    MybatisPlusConfig 分页拦截器

---
+++ src/main/java/com/example/demo/config/MybatisPlusConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @className: com.example.demo.config.MybatisPlusConfig
+ * @description: TODO
+ * @author: zxl
+ * @create: 2021-07-03 22:14
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
