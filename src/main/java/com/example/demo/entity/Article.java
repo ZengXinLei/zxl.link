567cb8be22e6df8aa2bb81b45583dda563ab55fc
ZengXinLei <46377981+ZengXinLei@users.noreply.github.com>
2021-07-04 21:43:29 +0800

    article 添加发布形式(publishType)和文章类型(articleType)

--- src/main/java/com/example/demo/entity/Article.java
+++ src/main/java/com/example/demo/entity/Article.java
@@ -1,8 +1,11 @@
 package com.example.demo.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import java.io.Serializable;
+import java.util.List;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -50,5 +53,23 @@ public class Article implements Serializable {
      */
     private Float time;
 
+    /**
+     * 发布形式 (1.公开,2.私密,3.好友可见）
+     */
+
+    private char publishType;
+
+
+    /**
+     * 文章类型(1.原创,2.转载,3.翻译）
+     */
+    private char articleType;
+
+
+    @TableField(exist = false)
+    private List<Tag> tags;
+
+    @TableField(exist = false)
+    private List<Category> categories;
 
 }
