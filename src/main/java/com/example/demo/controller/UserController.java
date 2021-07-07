d5735b1fc2a8e96319fe32196bbc80e4b96d0fd7
ZengXinLei <46377981+ZengXinLei@users.noreply.github.com>
2021-07-07 17:20:32 +0800

    获取用户基本信息

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,7 +3,9 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.User;
 import com.example.demo.util.R;
+import org.apache.tomcat.util.http.parser.Authorization;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -27,10 +29,16 @@ import java.util.Map;
 public class UserController extends BaseController {
 
 
-    @PreAuthorize("hasRole('ROLE_USER')")
-    @RequestMapping(value = "/t",method = RequestMethod.POST)
-    public R t(){
+//    @PreAuthorize("hasRole('ROLE_USER')")
+    @RequestMapping(value = "/baseInfo",method = RequestMethod.POST)
+    public R baseInfo(){
 
+        return R.ok().put("baseInfo",getUser());
+    }
+
+
+    @RequestMapping("/t")
+    public R t(){
         return R.ok();
     }
 }
