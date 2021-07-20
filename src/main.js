2698a72c04da53f1da03deb1af17d48c78611c88
ZengXinLei <46377981+ZengXinLei@users.noreply.github.com>
2021-07-21 04:53:44 +0800

    feat: 使用qs

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import VueRouter from 'vue-router'
 import router from "@/router/router";
+import request from '@/utils/request'
+import './permission'
 Vue.config.productionTip = false
+let qs=require("qs")
 
-
-
-
+Vue.prototype.$axios=request
+Vue.prototype.$qs=qs
 
 Vue.use(ElementUI)
 Vue.use(VueRouter)
