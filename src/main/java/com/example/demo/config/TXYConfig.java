c60bfdbf181aa5d7a94ab5d99e846605ad45b3b7
ZengXinLei <46377981+ZengXinLei@users.noreply.github.com>
2021-07-24 05:30:29 +0800

    feat: 腾讯云对象存储配置

--- src/main/java/com/example/demo/config/TXYConfig.java
+++ src/main/java/com/example/demo/config/TXYConfig.java
@@ -3,7 +3,9 @@ package com.example.demo.config;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-@Configuration
+import org.springframework.stereotype.Component;
+
+@Component
 @ConfigurationProperties(prefix ="txy" )
 @Data
 public class TXYConfig {
