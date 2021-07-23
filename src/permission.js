5fdf1672e0f10822f0183b18b7593342631d91f7
ZengXinLei <46377981+ZengXinLei@users.noreply.github.com>
2021-07-24 04:25:22 +0800

    update:跳转页面时删除所有事件监听

--- src/permission.js
+++ src/permission.js
@@ -1,5 +1,6 @@
 import router from '@/router/router'
 import store from '@/store'
+import eventListener from '@/store/modules/eventListener'
 router.beforeEach(((to, from, next) => {
   let fullPath = to.fullPath
   let reg=/^\/(.*?)\/index.*/g
@@ -9,6 +10,10 @@ router.beforeEach(((to, from, next) => {
   }
   store.commit("SET_ID",execArray[1])
 
+  store.state.eventListener.forEach(e=>{
+    document.removeEventListener(e.type,e.func)
+  })
+  console.log()
   let number = fullPath.search()
   next()
 }))
