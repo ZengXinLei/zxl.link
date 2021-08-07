03fb510f46f48c4380dd293e3002ef7534568abd
ZengXinLei <46377981+ZengXinLei@users.noreply.github.com>
2021-08-07 11:03:00 +0800

    update:更改登录界面

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,65 +1,78 @@
 <template>
-  <div class="login-container">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on"
-             label-position="left"
-    >
+  <div class="wrap">
 
-      <div class="title-container">
-        <h3 class="title">Login Form</h3>
+    <div v-if="!isActive" class="login_form">
+      <div class="login_title">{{ isRegister ? "注册" : "登录" }}</div>
+      <el-form style="margin-top: 20px;" class="form" :data="loginForm">
+
+        <div v-if="!isRegister">
+          <el-form-item style="display: block;">
+            <el-input v-model="loginForm.name" class="el-input" placeholder="用户名/邮箱"/>
+
+          </el-form-item>
+          <el-form-item  style="display: block;">
+            <el-input v-model="loginForm.password" class="el-input" type="password" placeholder="密码"/>
+
+          </el-form-item>
+          <div style="display: flex;flex-direction: row;justify-content: space-between;">
+            <el-form-item  style="display: block;">
+              <el-input v-model="loginForm.code" class="el-input" placeholder="验证码"></el-input>
+            </el-form-item>
+            <img v-if="loginForm.uuid!=='-1'" :src="$axios.defaults.baseURL+'user/getCode/'+loginForm.uuid"
+                 @click="getCode"/>
+          </div>
+          <el-form-item   style="display: block;">
+            <el-button :disabled="loading" v-loading="loading" @click="handleLogin" style="width: 100%;background: #ff7f5f;color: #FFFFFF">{{ isRegister ? "注册" : "登录" }}</el-button>
+
+          </el-form-item>
+        </div>
+        <div v-if="isRegister">
+          <el-form-item style="display: block;">
+            <el-input v-model="loginForm.email" class="el-input" placeholder="邮箱"/>
+
+          </el-form-item>
+          <el-form-item style="display: block;">
+            <el-input v-model="loginForm.name" class="el-input" placeholder="用户名"/>
+
+          </el-form-item>
+          <el-form-item style="display: block;">
+            <el-input v-model="loginForm.password" type="password" class="el-input" placeholder="密码"/>
+
+          </el-form-item>
+          <div style="display: flex;flex-direction: row;justify-content: space-between;">
+            <el-form-item  style="display: block;">
+              <el-input v-model="loginForm.code" class="el-input" placeholder="验证码"></el-input>
+            </el-form-item>
+            <img v-if="loginForm.uuid!=='-1'" :src="$axios.defaults.baseURL+'user/getCode/'+loginForm.uuid"
+                 @click="getCode"/>
+          </div>
+          <el-form-item   style="display: block;">
+            <el-button :disabled="loading" v-loading="loading" @click="handleLogin" style="width: 100%;background: #ff7f5f;color: #FFFFFF">{{ isRegister ? "注册" : "登录" }}</el-button>
+
+          </el-form-item>
+        </div>
+      </el-form>
+      <div class="for_reg_btn">
+<!--        <button class="button" @click="handleLogin">{{ isRegister ? "注册" : "登录" }}</button>-->
+        <span>{{ isRegister ? "还没有账号？" : "已有账号？" }}</span>
+        <a :href="`/login${isRegister?'':'?register=true'}`">{{ isRegister ? "马上登录" : "马上注册" }}</a>
       </div>
+    </div>
 
-      <el-form-item prop="username">
-        <span class="svg-container">
-          <svg-icon icon-class="user"/>
-        </span>
-        <el-input
-          ref="username"
-          v-model="loginForm.name"
-          placeholder="Username"
-          name="username"
-          type="text"
-          tabindex="1"
-          auto-complete="on"
-        />
-      </el-form-item>
+    <div v-else  class="info">
+      <h3>您的账号已经准备好了</h3>
 
-      <el-form-item prop="password">
-        <span class="svg-container">
-          <svg-icon icon-class="password"/>
-        </span>
-        <el-input
-          :key="passwordType"
-          ref="password"
-          v-model="loginForm.password"
-          :type="passwordType"
-          placeholder="Password"
-          name="password"
-          tabindex="2"
-          auto-complete="on"
-          @keyup.enter.native="handleLogin"
-        />
-        <span class="show-pwd" @click="showPwd">
-          <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'"/>
-        </span>
-      </el-form-item>
+      已向<b>{{ loginForm.email }}</b>
+      发送了一封电子邮件，其中包含激活您帐户的说明。激活链接仅在 48 小时内有效。如果您没有收到说明，请尝试检查您的垃圾邮件或垃圾邮件过滤器。
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;"
-                 @click.native.prevent="handleLogin"
-      >Login
-      </el-button>
-
-      <div class="tips">
-        <span style="margin-right:20px;">username: admin</span>
-        <span> password: any</span>
-      </div>
-
-    </el-form>
+    </div>
   </div>
 </template>
 
 <script>
-import { validUsername } from '@/utils/validate'
-import { setToken, setUser } from '@/utils/auth'
+import {validUsername} from '@/utils/validate'
+import {setToken, getToken,setUser} from '@/utils/auth'
+
 import Router from 'vue-router'
 import Layout from '@/layout'
 import componentMaps from '@/utils/components'
@@ -67,6 +80,12 @@ import asyncRouytes from '@/router/asyncRoutes'
 
 export default {
   name: 'Login',
+  mounted() {
+    // register
+    this.getCode()
+    this.isRegister = this.$route.query.register
+    console.log(this.$axios.defaults.baseURL)
+  },
   data() {
     const validateUsername = (rule, value, callback) => {
       if (!validUsername(value)) {
@@ -83,28 +102,49 @@ export default {
       }
     }
     return {
+      isActive:false,//是否已发送邮件
+      isRegister: false, //是注册还是登录
       loginForm: {
+        email:"",
         name: 'root',
-        password: '123'
+        password: '123',
+        uuid: "-1",
+        code: ""
       },
       loginRules: {
         // username: [{ required: true, trigger: 'blur', validator: validateUsername }],
         // password: [{ required: true, trigger: 'blur', validator: validatePassword }]
       },
-      loading: false,
+      loading: false,//
       passwordType: 'password',
       redirect: undefined
     }
   },
   watch: {
     $route: {
-      handler: function(route) {
+      handler: function (route) {
         this.redirect = route.query && route.query.redirect
       },
       immediate: true
     }
   },
   methods: {
+
+    /**
+     * 生成uuid
+     * @returns {string}
+     */
+    guid() {
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0,
+          v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+      });
+    },
+    getCode() {
+      let uuid = this.guid()
+      this.loginForm.uuid = uuid
+    },
     showPwd() {
       if (this.passwordType === 'password') {
         this.passwordType = ''
@@ -132,15 +172,33 @@ export default {
       //   }
       // })
       this.loading = true
-      this.$axios.post('/login', this.$qs.stringify({
+      this.$axios.post(this.isRegister ? '/user/register' : '/login', this.$qs.stringify({
 
         ...this.loginForm
       })).then(res => {
 
-        setToken(res.data.token)
-        this.loading = false
+        if(res.data.code!==0)
+        {
+          this.$message({
+            type:"error",
+            message:res.data.msg
+          })
+          this.loading=false
+          return
+        }
+        if(this.isRegister){
+          this.isActive=true
+          this.loading = false
 
-        this.$router.push('/editor')
+        }else {
+          setToken(res.data.token)
+            this.loading = false
+
+            console.log(getToken())
+            this.$router.push('/editor')
+        }
+
+
 
       })
     },
@@ -150,113 +208,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
-/* 修复input 背景不协调 和光标变色 */
-/* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
+<style lang="less">
 
-$bg: #283443;
-$light_gray: #fff;
-$cursor: #fff;
-
-@supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
-  .login-container .el-input input {
-    color: $cursor;
-  }
-}
-
-/* reset element-ui css */
-.login-container {
-  .el-input {
-    display: inline-block;
-    height: 47px;
-    width: 85%;
-
-    input {
-      background: transparent;
-      border: 0px;
-      -webkit-appearance: none;
-      border-radius: 0px;
-      padding: 12px 5px 12px 15px;
-      color: $light_gray;
-      height: 47px;
-      caret-color: $cursor;
-
-      &:-webkit-autofill {
-        box-shadow: 0 0 0px 1000px $bg inset !important;
-        -webkit-text-fill-color: $cursor !important;
-      }
-    }
-  }
-
-  .el-form-item {
-    border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
-    border-radius: 5px;
-    color: #454545;
-  }
-}
-</style>
-
-<style lang="scss" scoped>
-$bg: #2d3a4b;
-$dark_gray: #889aa4;
-$light_gray: #eee;
-
-.login-container {
-  min-height: 100%;
-  width: 100%;
-  background-color: $bg;
-  overflow: hidden;
-
-  .login-form {
-    position: relative;
-    width: 520px;
-    max-width: 100%;
-    padding: 160px 35px 0;
-    margin: 0 auto;
-    overflow: hidden;
-  }
-
-  .tips {
-    font-size: 14px;
-    color: #fff;
-    margin-bottom: 10px;
-
-    span {
-      &:first-of-type {
-        margin-right: 16px;
-      }
-    }
-  }
-
-  .svg-container {
-    padding: 6px 5px 6px 15px;
-    color: $dark_gray;
-    vertical-align: middle;
-    width: 30px;
-    display: inline-block;
-  }
-
-  .title-container {
-    position: relative;
-
-    .title {
-      font-size: 26px;
-      color: $light_gray;
-      margin: 0px auto 40px auto;
-      text-align: center;
-      font-weight: bold;
-    }
-  }
-
-  .show-pwd {
-    position: absolute;
-    right: 10px;
-    top: 7px;
-    font-size: 16px;
-    color: $dark_gray;
-    cursor: pointer;
-    user-select: none;
-  }
-}
+@import "index";
 </style>
