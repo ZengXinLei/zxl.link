43bdec0aaf5424993d97625023e09e3cd4dd5eb2
ZengXinLei <46377981+ZengXinLei@users.noreply.github.com>
2021-08-07 11:03:55 +0800

    fix:修复有时登录失败的bug

--- src/utils/request.js
+++ src/utils/request.js
@@ -9,7 +9,7 @@ const service = axios.create({
   baseURL: "http://localhost:8080/", // url = base url + request url
   // baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
-  timeout: 5000 // request timeout
+  timeout: 50000 // request timeout
 })
 
 // request interceptor
@@ -18,7 +18,7 @@ service.interceptors.request.use(
     // do something before request is sent
 
     // debugger
-    if (store.getters.token) {
+    if (getToken()) {
       // let each request carry token
       // ['X-Token'] is a custom headers key
       // please modify it according to the actual situation
@@ -68,16 +68,9 @@ service.interceptors.response.use(
 
 
     }
-    else if(res.code===500){
-      Message({
-        message: res.msg,
-        type: 'error',
-        duration: 5 * 1000
-      })
-      return Promise.reject(response)
-    }
+
     else {
-      if(res.msg!=="success"){
+      if(res.code===0&&res.msg!=="success"){
         Message({
           message: res.msg,
           type: 'success',
