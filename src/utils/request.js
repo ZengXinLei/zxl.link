973a85e6b57678288a231caf59a28b823e9cf223
ZengXinLei <46377981+ZengXinLei@users.noreply.github.com>
2021-08-09 16:15:02 +0800

    feat: 我的信息页面

--- src/utils/request.js
+++ src/utils/request.js
@@ -7,6 +7,7 @@ import router from '@/router'
 // create an axios instance
 const service = axios.create({
   baseURL: "http://localhost:8080/", // url = base url + request url
+  // baseURL: "http://www.zxl.link:8080/", // url = base url + request url
   // baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 50000 // request timeout
---
+++ src/views/personal/info/index.vue
@@ -0,0 +1,61 @@
+<template>
+
+  <div style="display: flex;flex-direction: row;justify-content: center;padding-top: 30px">
+
+    <el-card :body-style="{width:'600px'}">
+      <el-form  :model="dataForm" label-width="80px" >
+        <el-form-item label="头像">
+          <el-upload
+            class="upload-demo"
+            drag
+
+
+            action="https://jsonplaceholder.typicode.com/posts/"
+            multiple>
+            <i class="el-icon-upload"></i>
+            <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+            <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+          </el-upload>
+        </el-form-item>
+        <el-form-item label="昵称">
+          <el-input style="width: 200px;" placeholder="你的昵称"></el-input>
+        </el-form-item>
+        <el-form-item label="签名">
+          <el-input style="width: 400px;" type="textarea" placeholder="请输入内容" :rows="3"></el-input>
+        </el-form-item>
+        <el-form-item label="性别">
+          <el-radio-group v-model="dataForm.sex">
+            <el-radio :label="1">男</el-radio>
+            <el-radio :label="2">女</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="出生日期">
+          <el-date-picker
+            v-model="dataForm.birth"
+            type="date"
+            placeholder="选择日期">
+          </el-date-picker>
+        </el-form-item>
+      </el-form>
+
+    </el-card>
+
+  </div>
+</template>
+
+<script>
+export default {
+name: "index",
+  data(){
+  return{
+    dataForm:{
+
+    }
+  }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
